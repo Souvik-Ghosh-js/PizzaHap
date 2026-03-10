4440dc6c612c4e707fe2d454c53f95513458ee62
Souvik Ghosh <souvik_ghosh@pursuitsoftware.com>
2026-03-10 14:34:33 +0530

    prod url

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://yourdomain.com']
+    ? ['http://13.232.73.121']
     : '*',
   credentials: true,
 }));
